b5fef9f31f67e16ecaa81c519c81cdc4f52fcfbf
lei <1903696084@qq.com>
2025-08-17 11:32:00 +0800

    base c++ project

---
+++ include/math.h
@@ -0,0 +1,11 @@
+#pragma once
+
+template <typename T>
+T add(T a, T b) {
+    return a + b;
+}
+
+template <typename T>
+T subtract(T a, T b) {
+    return a - b;
+}
---
+++ src/main.cc
@@ -0,0 +1,11 @@
+#include "math.h"
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int c = add(a, b);
+    // change 5
+    // new_branch conflict
+    // new_branch conflict
+    return 0;
+}
